a0e3867e9e8fd28d97f2f58c2c9cd40d0d7db1d1
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-20 22:08:20 -0800

    Add FindEToTheNthDigit

---
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/FindEToTheNthDigit.java
@@ -0,0 +1,16 @@
+package io.github.nathanvegetable.numbers;
+
+/**
+ * @author Nathan Gutauckas
+ */
+public class FindEToTheNthDigit {
+
+	public static void main(String[] args) {
+		double result = FindEToTheNthDigit.find(Math.PI, 10);
+		System.out.println(result);
+	}
+
+	public static double find(double input, int digits) {
+		// TODO: FindEToTheNthDigit
+	}
+}
