54bccd3af5dfdd3718213c0974a2555580ddab02
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-20 22:55:21 -0800

    Add NextPrimeNumber

---
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/NextPrimeNumber.java
@@ -0,0 +1,32 @@
+package io.github.nathanvegetable.numbers;
+
+/**
+ * Project: Have the program find prime numbers until the user chooses to stop
+ * asking for the next one.
+ * 
+ * @author NathanVegetable
+ *
+ */
+public class NextPrimeNumber {
+
+	public static void main(String[] args) {
+		int nextNumber = NextPrimeNumber.findNextPrimeNumber(1005);
+		System.out.println(nextNumber);
+	}
+
+	public static int findNextPrimeNumber(int input) {
+		int nextNum = input++;
+		while (!isPrimeNumber(nextNum))
+			nextNum++;
+		return nextNum;
+	}
+
+	public static boolean isPrimeNumber(int input) {
+		if (input < 2)
+			return false;
+		for (int i = 2; i < input; i++)
+			if (input % i == 0)
+				return false;
+		return true;
+	}
+}
