05cede9de40d8af53546336e7ff14a6f58f1489a
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-20 22:23:18 -0800

    Auto-formatting

--- PracticalProjects/src/io/github/nathanvegetable/numbers/TestNumbers.java
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/TestNumbers.java
@@ -21,20 +21,21 @@ class TestNumbers {
 	@Test
 	void piMathShouldMatchHardcodePI14Digit() {
 		double hardcodedPI14Digit = 3.14159265358979d;
-		assertEquals(hardcodedPI14Digit, FindPiToTheNthDigit.findWithMathPI(FindPiToTheNthDigit.MATH_PI_MAX_PRECISION - 1));
+		assertEquals(hardcodedPI14Digit,
+				FindPiToTheNthDigit.findWithMathPI(FindPiToTheNthDigit.MATH_PI_MAX_PRECISION - 1));
 	}
-	
+
 	@Test
 	void findToNthDigitShouldMatchHardcode() {
 		double hardcodedPI3Digit = 3.14d;
-		assertEquals(hardcodedPI3Digit,FindEToTheNthDigit.findWithStringFormat(Math.PI, 2));
+		assertEquals(hardcodedPI3Digit, FindEToTheNthDigit.findWithStringFormat(Math.PI, 2));
 	}
-	
+
 	@Test
 	void fibonacciSequence7thDigitShouldBe8() {
 		assertEquals(8, FibonacciSequence.getSequence(7)[6]);
 	}
-	
+
 	@Test
 	void fibonacciSequence20thDigitShouldBe4181() {
 		assertEquals(4181, FibonacciSequence.getSequence(20)[19]);
