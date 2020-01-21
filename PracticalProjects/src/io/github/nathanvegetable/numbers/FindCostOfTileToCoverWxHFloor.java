6c77c614464c8a14601128d75b7079d3b758bcb8
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-20 23:22:47 -0800

    Add FindCostOfTileToCoverWxHFloor

---
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/FindCostOfTileToCoverWxHFloor.java
@@ -0,0 +1,24 @@
+package io.github.nathanvegetable.numbers;
+
+/**
+ * Project: Find Cost of Tile to Cover W x H Floor - Calculate the total cost of
+ * tile it would take to cover a floor plan of width and height, using a cost
+ * entered by the user.
+ * 
+ * @author NathanVegetable
+ *
+ */
+public class FindCostOfTileToCoverWxHFloor {
+	public final int DEFAULT_TILE_WIDTH = 12;
+	public final int DEFAULT_TILE_HEIGHT = 12;
+	public final int DEFAULT_FLOOR_PLAN_WIDTH = 1500;
+	public final int DEFAULT_FLOOR_PLAN_HEIGHT = 1000;
+
+	public double findCostOfFloorPlan(double tileCost, int tileWidth, int tileHeight, int floorPlanWidth,
+			int floorPlanHeight) {
+		double tilesWidthNeeded = Math.ceil((double) floorPlanWidth / (double) tileWidth);
+		double tilesHeightNeeded = Math.ceil((double) floorPlanHeight / (double) tileHeight);
+		double tilesNeeded = tilesWidthNeeded * tilesHeightNeeded;
+		return tileCost * tilesNeeded;
+	}
+}
