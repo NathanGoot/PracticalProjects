5f8c726b4abcf9ae6f009a2307508fc5191ebdad
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-20 21:25:59 -0800

    Rename findWithLimit to findWithMathPI

--- PracticalProjects/src/io/github/nathanvegetable/numbers/FindPiToTheNthDigit.java
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/FindPiToTheNthDigit.java
@@ -4,10 +4,10 @@ public class FindPiToTheNthDigit {
 	public static int DEFAULT_LIMIT = 10;
 	
 	public static void main(String[] args) {
-		FindPiToTheNthDigit.findWithLimit(DEFAULT_LIMIT);
+		FindPiToTheNthDigit.findWithMathPI(DEFAULT_LIMIT);
 	}
 
-	public static double findWithLimit(int limit) {
+	public static double findWithMathPI(int limit) {
 		double pi = Math.PI;
 		String piString = String.valueOf(pi).replace(".", "");
 		piString = piString.substring(0, limit + 1);
