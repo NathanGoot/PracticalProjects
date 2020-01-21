f3d9ab7428bf71df15109c242e94a1849b625a09
Nathan Gutauckas <veget00tegev@yahoo.com>
2020-01-20 21:14:50 -0800

    Add initial FindPiToTheNthDigit

---
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/FindPiToTheNthDigit.java
@@ -0,0 +1,11 @@
+package io.github.nathanvegetable.numbers;
+
+public class FindPiToTheNthDigit {
+	public static void main(String[] args) {
+		FindPiToTheNthDigit.findWithLimit(100);
+	}
+
+	public static int findWithLimit(int limit) {
+		// TODO: find Pi
+	}
+}
