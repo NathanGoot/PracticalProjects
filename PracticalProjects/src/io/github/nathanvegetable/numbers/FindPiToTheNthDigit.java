ca38e79130c585b61fa15ff3715948db11832777
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-20 21:25:03 -0800

    Add DEFAULT_LIMIT as 10

--- PracticalProjects/src/io/github/nathanvegetable/numbers/FindPiToTheNthDigit.java
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/FindPiToTheNthDigit.java
@@ -1,8 +1,10 @@
 package io.github.nathanvegetable.numbers;
 
 public class FindPiToTheNthDigit {
+	public static int DEFAULT_LIMIT = 10;
+	
 	public static void main(String[] args) {
-		FindPiToTheNthDigit.findWithLimit(10);
+		FindPiToTheNthDigit.findWithLimit(DEFAULT_LIMIT);
 	}
 
 	public static double findWithLimit(int limit) {
