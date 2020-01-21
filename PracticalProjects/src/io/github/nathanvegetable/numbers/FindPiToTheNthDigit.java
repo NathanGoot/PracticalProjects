2bc6758fbbaa4b602bd284aec625c814cbe9a7fb
Nathan Gutauckas <veget00tegev@yahoo.com>
2020-01-20 21:20:12 -0800

    Use Math.PI and substring to determine PI digits

--- PracticalProjects/src/io/github/nathanvegetable/numbers/FindPiToTheNthDigit.java
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/FindPiToTheNthDigit.java
@@ -2,10 +2,13 @@ package io.github.nathanvegetable.numbers;
 
 public class FindPiToTheNthDigit {
 	public static void main(String[] args) {
-		FindPiToTheNthDigit.findWithLimit(100);
+		FindPiToTheNthDigit.findWithLimit(10);
 	}
 
 	public static int findWithLimit(int limit) {
-		// TODO: find Pi
+		double pi = Math.PI;
+		String piString = String.valueOf(pi).replace(".", "");
+		piString = piString.substring(0, limit + 1);
+		return Integer.parseInt(piString);
 	}
 }
