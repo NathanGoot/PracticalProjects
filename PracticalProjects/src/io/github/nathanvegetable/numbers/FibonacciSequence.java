ee65f5e87ddffa395c8d03428ce66e7a4eac10e3
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-20 22:20:20 -0800

    Add FibonacciSequence

---
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/FibonacciSequence.java
@@ -0,0 +1,21 @@
+package io.github.nathanvegetable.numbers;
+
+public class FibonacciSequence {
+
+	public static void main(String[] args) {
+		int[] result = FibonacciSequence.getSequence(10);
+		for (int num : result)
+			System.out.println("" + num);
+	}
+
+	public static int[] getSequence(int digits) {
+		if (digits <= 0)
+			throw new IllegalArgumentException("digits must be > 0");
+		int[] sequence = new int[digits];
+		if (digits > 1)
+			sequence[1] = 1;
+		for (int i = 2; i < sequence.length; i++)
+			sequence[i] = sequence[i - 1] + sequence[i - 2];
+		return sequence;
+	}
+}
