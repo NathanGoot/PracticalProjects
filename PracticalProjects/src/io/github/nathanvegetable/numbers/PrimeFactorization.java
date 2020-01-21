3cfc0766dcf6eeeecd12aff07cceb024c97f3c1f
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-20 22:33:58 -0800

    Add PrimeFactorization

---
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/PrimeFactorization.java
@@ -0,0 +1,20 @@
+package io.github.nathanvegetable.numbers;
+
+import java.util.ArrayList;
+
+/**
+ * Project: Have the user enter a number and find all Prime Factors (if there
+ * are any) and display them.
+ * 
+ * @author Nathan
+ *
+ */
+public class PrimeFactorization {
+
+	public static void main(String[] args) {
+	}
+
+	public ArrayList<Integer> findPrimeFactors(int input) {
+		// TODO: findPrimeFactors
+	}
+}
