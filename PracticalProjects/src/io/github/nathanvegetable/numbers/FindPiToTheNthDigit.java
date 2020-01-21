5e3c0936722249da607bb4fbc70e17e53c442058
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-20 21:28:27 -0800

    Throw IllegalArgumentException if limit is outside of expected bounds

--- PracticalProjects/src/io/github/nathanvegetable/numbers/FindPiToTheNthDigit.java
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/FindPiToTheNthDigit.java
@@ -2,12 +2,15 @@ package io.github.nathanvegetable.numbers;
 
 public class FindPiToTheNthDigit {
 	public static int DEFAULT_LIMIT = 10;
-	
+
 	public static void main(String[] args) {
 		FindPiToTheNthDigit.findWithMathPI(DEFAULT_LIMIT);
 	}
 
 	public static double findWithMathPI(int limit) {
+		// Math.PI has only precision up to 15 digits.
+		if (limit < 0 || limit > 15)
+			throw new IllegalArgumentException("limit must be > 0 && <= 15");
 		double pi = Math.PI;
 		String piString = String.valueOf(pi).replace(".", "");
 		piString = piString.substring(0, limit + 1);
