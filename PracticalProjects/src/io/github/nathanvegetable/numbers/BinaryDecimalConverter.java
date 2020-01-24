f1551932c363e91dffc28612e3ff4ceff7bca5bf
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-23 21:29:18 -0800

    Add BinaryDecimalConverter with placeholder methods

---
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/BinaryDecimalConverter.java
@@ -0,0 +1,27 @@
+package io.github.nathanvegetable.numbers;
+
+import java.util.ArrayList;
+
+/**
+ * Project: Binary to Decimal and Back Converter - Develop a converter to
+ * convert a decimal number to binary or a binary number to its decimal
+ * equivalent.
+ * 
+ * @author NathanVegetable
+ *
+ */
+public class BinaryDecimalConverter {
+	public static void main(String[] args) {
+
+	}
+
+	public static long binaryToDecimal(long decimalValue) {
+		// TODO
+		return -1;
+	}
+
+	public static long decimalToBinary(long binaryValue) {
+		// TODO
+		return -1;
+	}
+}
