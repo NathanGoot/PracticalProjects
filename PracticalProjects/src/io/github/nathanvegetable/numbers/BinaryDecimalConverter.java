0d770fbdbc3d7dfd90d14375269a9d66cf81f2d7
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-23 21:34:24 -0800

    Add decimalToBinary logic

--- PracticalProjects/src/io/github/nathanvegetable/numbers/BinaryDecimalConverter.java
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/BinaryDecimalConverter.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  */
 public class BinaryDecimalConverter {
 	public static void main(String[] args) {
-
+		
 	}
 
 	public static long binaryToDecimal(long decimalValue) {
@@ -21,7 +21,29 @@ public class BinaryDecimalConverter {
 	}
 
 	public static long decimalToBinary(long binaryValue) {
-		// TODO
-		return -1;
+		// Represent 1 as TRUE, 0 as FALSE
+		ArrayList<Boolean> decimalRepresentation = new ArrayList<Boolean>();
+		decimalRepresentation.add(Boolean.FALSE);
+		while (binaryValue > 0) {
+			boolean foundEmptyDecimal = false;
+			for (int index = decimalRepresentation.size() - 1; index >= 0 && !foundEmptyDecimal; index--) {
+				if (!decimalRepresentation.get(index)) {
+					decimalRepresentation.set(index, Boolean.TRUE);
+					binaryValue--;
+					foundEmptyDecimal = true;
+				}
+			}
+			if (!foundEmptyDecimal) {
+				// There was no 0 found to increment. Take the leftmost decimal, switch it to 0,
+				// and add a new leftmost 1
+				decimalRepresentation.set(0, Boolean.FALSE);
+				decimalRepresentation.add(0, Boolean.TRUE);
+				binaryValue--;
+			}
+		}
+		String decimalsInString = "";
+		for (Boolean decimal : decimalRepresentation)
+			decimalsInString += decimal ? "1" : "0";
+		return Long.parseLong(decimalsInString);
 	}
 }
