39abd800dabe257b640498f7a7b7baf40504d9b0
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-25 13:57:19 -0800

    Add calculator display to display numbers

--- PracticalProjects/src/io/github/nathanvegetable/numbers/Calculator.java
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/Calculator.java
@@ -1,9 +1,13 @@
 package io.github.nathanvegetable.numbers;
 
+import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * Project: Calculator - A simple calculator to do basic operators. Make it a
@@ -14,6 +18,8 @@ import javax.swing.JPanel;
  */
 public class Calculator {
 	JFrame mainFrame = null;
+	JTextField display = null;
+	final static Font DISPLAY_FONT = new Font("serif", Font.PLAIN, 20);
 
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
@@ -30,7 +36,14 @@ public class Calculator {
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout(5, 5));
 		mainFrame.add(mainPanel);
+
+		display = new JTextField("0", 12);
+		display.setEditable(false);
+		display.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		display.setFont(DISPLAY_FONT);
+		mainPanel.add(display);
 
 		mainFrame.pack();
 		// Set to center of screen
