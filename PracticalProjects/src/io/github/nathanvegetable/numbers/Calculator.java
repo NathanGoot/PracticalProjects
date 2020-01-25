d18b68e12dd1ee4a64ddd1db4e4687d1ca7809ac
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-25 13:47:57 -0800

    Setup initial frame/panel

--- PracticalProjects/src/io/github/nathanvegetable/numbers/Calculator.java
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/Calculator.java
@@ -1,5 +1,10 @@
 package io.github.nathanvegetable.numbers;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  * Project: Calculator - A simple calculator to do basic operators. Make it a
  * scientific calculator for added complexity.
@@ -8,6 +13,31 @@ package io.github.nathanvegetable.numbers;
  *
  */
 public class Calculator {
+	JFrame mainFrame = null;
+
 	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		calculator.display();
+	}
+
+	public Calculator() {
+		init();
+	}
+
+	public void init() {
+		mainFrame = new JFrame("Calculator");
+		mainFrame.setPreferredSize(new Dimension(300, 500));
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel mainPanel = new JPanel();
+		mainFrame.add(mainPanel);
+
+		mainFrame.pack();
+		// Set to center of screen
+		mainFrame.setLocationRelativeTo(null);
+	}
+
+	public void display() {
+		mainFrame.setVisible(true);
 	}
 }
