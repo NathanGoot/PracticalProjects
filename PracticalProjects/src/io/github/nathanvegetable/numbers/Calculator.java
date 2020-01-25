dfc771a1b8c1945a7325811ff18a3a74452d1b15
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-24 21:43:35 -0800

    Add empty Calculator

---
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/Calculator.java
@@ -0,0 +1,13 @@
+package io.github.nathanvegetable.numbers;
+
+/**
+ * Project: Calculator - A simple calculator to do basic operators. Make it a
+ * scientific calculator for added complexity.
+ * 
+ * @author NathanVegetable
+ *
+ */
+public class Calculator {
+	public static void main(String[] args) {
+	}
+}
