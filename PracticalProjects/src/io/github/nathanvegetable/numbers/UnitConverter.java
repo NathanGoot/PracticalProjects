d929993948bcbf283fde6dd7a12e4a5297380689
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-30 01:02:36 -0800

    Update Doc to indicate we have switched from one Penny to Dollar as base

--- PracticalProjects/src/io/github/nathanvegetable/numbers/UnitConverter.java
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/UnitConverter.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  * 
  * @author NathanVegetable
  * 
- *         I have decided to use one penny as the base value (1).
+ *         I have decided to use one dollar as the base value (1).
  *
  */
 public class UnitConverter {
