f3aea5a050fba00d00f291832a55d5d853c5da85
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-29 22:34:36 -0800

    Add Currency with PENNY to HUNDRED_DOLLAR_BILL

--- PracticalProjects/src/io/github/nathanvegetable/numbers/UnitConverter.java
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/UnitConverter.java
@@ -7,11 +7,23 @@ package io.github.nathanvegetable.numbers;
  * program will then make the conversion.
  * 
  * @author NathanVegetable
+ * 
+ *         I have decided to use one penny as the base value (1).
  *
  */
 public class UnitConverter {
-	
+
 	public static void main(String[] args) {
 
+	}
+
+	public static enum Currency implements Unit {
+		PENNY(1), NICKEL(5), DIME(10), QUARTER(25), HALF_DOLLAR(50), DOLLAR(100), TEN_DOLLAR_BILL(
+				1000), TWENTY_DOLLAR_BILL(2000), FIFTY_DOLLAR_BILL(5000), HUNDRED_DOLLAR_BILL(10000);
+		long value;
+
+		Currency(long value) {
+			this.value = value;
+		}
 	}
 }
