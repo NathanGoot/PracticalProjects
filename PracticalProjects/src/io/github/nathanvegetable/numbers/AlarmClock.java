33e166c6caa788daf1de45e08a7f3b7f300b056c
Nathan Gutauckas <nathan.gutauckas@gmail.com>
2020-01-31 22:32:13 -0800

    Add empty AlarmClock

---
+++ PracticalProjects/src/io/github/nathanvegetable/numbers/AlarmClock.java
@@ -0,0 +1,23 @@
+package io.github.nathanvegetable.numbers;
+
+/**
+ * Project: Alarm Clock - A simple clock where it plays a sound after X number
+ * of minutes/seconds or at a particular time.
+ * 
+ * @author NathanVegetable
+ *
+ */
+public class AlarmClock {
+
+	public static void main(String[] args) {
+		new AlarmClock();
+	}
+
+	public AlarmClock() {
+		init();
+	}
+
+	public void init() {
+
+	}
+}
